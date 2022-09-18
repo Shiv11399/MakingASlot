40604889e961bc4301aa8d272b57cab7f9d47099
Shiv Bajpai <shiv.bajpai.5@gmail.com>
2022-09-18 21:58:13 +0530

    Pushing Project

---
+++ Assets/Scripts/Machine.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace Slot
+{
+    enum MachineState
+    {
+        Spinning = 0,
+        Stopped = 1,
+    }
+    public class Machine : MonoBehaviour
+    {
+        public Roll[] rolls;
+        [SerializeField] private Image SpinAndStopButton;
+        [SerializeField] private Sprite StopSprite;
+        [SerializeField] private Sprite StartSprite;
+        private MachineState CurrentState;
+        private int stopCount = 0;
+
+        private void OnEnable()
+        {
+            Roll.ReelStopped += RellStopped;
+
+        }
+
+        private void RellStopped()
+        {
+            stopCount++;
+            if(stopCount == 5)
+            {
+                SpinAndStopButton.sprite = StartSprite;
+                CurrentState = MachineState.Stopped;
+                stopCount = 0;
+            }
+        }
+
+        public void SymbolButtonPressed(int Symbol)
+        {
+            foreach (Roll R in rolls)
+            {
+                float spinTime = Random.Range(3, 5);
+                R.SetStop((RollIconState)Symbol, spinTime);
+            }
+            CurrentState = MachineState.Spinning;
+            SpinAndStopButton.sprite = StopSprite;
+        }
+        public void RandomSpin()
+        {
+            if (CurrentState == MachineState.Spinning)
+            {
+                foreach (Roll R in rolls)
+                {
+                    R.ForceStop();
+                }
+                return;
+            }
+
+            foreach (Roll R in rolls)
+            {
+                float spinTime = Random.Range(3, 5);
+                int randomSymbol = Random.Range(0, 8);
+                R.SetStop((RollIconState)randomSymbol, spinTime);
+            }
+            CurrentState = MachineState.Spinning;
+            SpinAndStopButton.sprite = StopSprite;
+        }
+
+        private void CheckStateAndStop()
+        {
+           
+        }
+    }
+}
---
+++ Assets/Scripts/Roll.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+namespace Slot
+{
+    public enum RollState
+    {
+        Spnning = 0,
+        Stopped = 1,
+    }
+    public enum RollIconState
+    {
+        J = 0,
+        GreenFish = 1,
+        final = 2,
+        Wild = 3,
+        Star = 4,
+        Bonus = 5,
+        A = 6,
+        Q = 7,
+        K = 8,
+
+    }
+    public class Roll : MonoBehaviour
+    {
+
+
+        private RollIconState CurrentSymbol = RollIconState.J;
+        private RollIconState LastSymbol = RollIconState.K;
+        private RollState rollState = RollState.Stopped;
+        [SerializeField] float rollSpeed;
+        [SerializeField] Transform[] RollSegments;
+        [SerializeField] double totoalRollSpined = 0;
+        [SerializeField] double spinningError = 0;
+        RollIconState stopIcon;
+
+        public static UnityAction ReelStopped;
+
+        bool isChecking = false;
+
+        public void SetStop(RollIconState symbol, float time)
+        {
+            rollState = RollState.Spnning;
+            StartCoroutine(StopState(time));
+            stopIcon = symbol;
+        }
+        public void ForceStop()
+        {
+            isChecking = true;
+        }
+        IEnumerator StopState(float time)
+        {
+            yield return new WaitForSeconds(time);
+            isChecking = true;
+
+        }
+        private void Update()
+        {
+            if (isChecking)
+            {
+                StopRolling(stopIcon);
+                ReelStopped?.Invoke();
+            }
+        }
+        private void FixedUpdate()
+        {
+            if (rollState != RollState.Spnning) return;
+
+            foreach (Transform roll in RollSegments)
+            {
+                var pos = roll.position;
+                roll.position = new Vector3(pos.x, pos.y - rollSpeed * Time.deltaTime, pos.z);
+                totoalRollSpined += Time.fixedDeltaTime * rollSpeed;
+                if (totoalRollSpined >= 7.5)
+                {
+                    spinningError = totoalRollSpined - 7.5f;
+                    totoalRollSpined = spinningError;
+                    CheckCurrentSymbol();
+                }
+            }
+
+                for (int i = 0; i < RollSegments.Length; i++)
+                {
+                    int r = (i < RollSegments.Length - 1) ? (i + 1) : 0;
+                    if (RollSegments[i].position.y < -7.5)
+                    {
+                        var afterRoll = RollSegments[r].position;
+                        RollSegments[i].position = new Vector3(afterRoll.x, afterRoll.y + 22.5f, afterRoll.z);
+
+                    }
+                }
+            
+        }
+
+        private void StopRolling(RollIconState currentSymbol)
+        {
+            if (CurrentSymbol == currentSymbol)
+            {
+                rollState = RollState.Stopped;
+                isChecking = false;
+            }
+        }
+
+        private void CheckCurrentSymbol()
+        {
+            if (CurrentSymbol == LastSymbol)
+            {
+                CurrentSymbol = 0;
+                return;
+            }
+            CurrentSymbol++;
+        }
+    }
+}
